24822c2d803dc5d3390c5ac1dae2d7de454dfac3
Przemek Jozwiak <pjozwiak10@gmail.com>
2019-12-29 12:49:28 +0100

    Updates

---
+++ precache-manifest.59eb9421370fb452f0dcfdd71fc30e73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfddc2464ac4b0b54b5141fa52ab6cf8",
+    "url": "/Project1/index.html"
+  },
+  {
+    "revision": "1b9555848ac42167dfea",
+    "url": "/Project1/static/css/main.908a3450.chunk.css"
+  },
+  {
+    "revision": "aebb9472bebd5b896be8",
+    "url": "/Project1/static/js/2.2dec7508.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Project1/static/js/2.2dec7508.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b9555848ac42167dfea",
+    "url": "/Project1/static/js/main.6066aeb1.chunk.js"
+  },
+  {
+    "revision": "9ca507cd153ccba4656f",
+    "url": "/Project1/static/js/runtime-main.b8cb2387.js"
+  },
+  {
+    "revision": "f8b8d20ad0f1a8c1584ccc392197c867",
+    "url": "/Project1/static/media/background.f8b8d20a.jpg"
+  },
+  {
+    "revision": "fbcd9296ac7170bb32534ef9f2e2611a",
+    "url": "/Project1/static/media/racing.fbcd9296.svg"
+  },
+  {
+    "revision": "d176bd3a3907e9529afe524208f297c3",
+    "url": "/Project1/static/media/wheel.d176bd3a.svg"
+  }
+]);
